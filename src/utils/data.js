f547a7442e8da0a62de440722564a6c7251911d2
Alejandro <hansalejandroca@hotmail.com>
2022-12-27 17:22:08 -0500

    tercer Commit

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        name: 'adidas',
+        price: 70,
+        url: 'f1.jpg'
+    },
+    {
+        name: 'Microfono',
+        price: 70,
+        url: 'f2.jpg'
+    },
+    {
+        name: 'Mouse Gamer',
+        price: 30,
+        url: 'f3.jpg'
+    },
+    {
+        name: 'Monitor',
+        price: 400,
+        url: 'f4.jpg'
+    },
+    {
+        name: 'Silla Gaming',
+        price: 900,
+        url: 'f5.jpg'
+    },
+    {
+        name: 'PC Gamer',
+        price: 1200,
+        url: 'f6.jpg'
+    }
+]
